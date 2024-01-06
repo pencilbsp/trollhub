18fe385e1562b55dc45f80a3d2eea2f4f14dd170
pencilbsp <pencil.bsp@gmail.com>
2024-01-06 18:14:35 +0700

    increment lazy image threshold

--- src/app/(root)/chapter/[slug]/page.tsx
+++ src/app/(root)/chapter/[slug]/page.tsx
@@ -50,7 +50,7 @@ export default async function ChapterPage({ params }: Props) {
                   alt=""
                   effect="blur"
                   tmpRatio="1/1"
-                  threshold={500}
+                  threshold={1000}
                   key={chapter.id + index}
                   src={`${pathname}${search}`}
                 />
