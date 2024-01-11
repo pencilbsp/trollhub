9e9bbf9ea983eee54a23333055933f835d759d3a
pencilbsp <pencil.bsp@gmail.com>
2024-01-11 10:24:45 +0700

    view in container

--- src/app/(root)/chapter/[slug]/page.tsx
+++ src/app/(root)/chapter/[slug]/page.tsx
@@ -30,10 +30,12 @@ export default async function ChapterPage({ params }: Props) {
 
   if (chapter.mobileOnly && chapter.type === "comic")
     return (
-      <div className="border border-dashed px-4 py-8 flex flex-col gap-4 items-center justify-center rounded-xl">
-        <p className="text-lg font-semibold text-center">
-          Nội dung không khả dụng ngay bây giờ, vui lòng quay lại sau. Xin cám ơn!
-        </p>
+      <div className="container px-4 xl:max-w-6xl">
+        <div className="border border-dashed px-4 py-8 flex flex-col gap-4 items-center justify-center rounded-xl">
+          <p className="text-lg font-semibold text-center">
+            Nội dung không khả dụng ngay bây giờ, vui lòng quay lại sau. Xin cám ơn!
+          </p>
+        </div>
       </div>
     )
 
