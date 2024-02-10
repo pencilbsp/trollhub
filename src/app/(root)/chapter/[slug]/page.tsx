55d0c3cfc3ffb0c8cb021f5a47d7dfd9161814ab
pencilbsp <pencil.bsp@gmail.com>
2024-02-10 10:51:59 +0700

    update new stream image routes

--- src/app/(root)/chapter/[slug]/page.tsx
+++ src/app/(root)/chapter/[slug]/page.tsx
@@ -29,7 +29,7 @@ async function getImages(chapter: NonNullable<Awaited<ReturnType<typeof getChapt
     const response = await fetch(`${USER_CONTENTS_HOST}/api/fttps:webp/${chapter.fid}`)
     const data = await response.json()
     if (data.images.length) {
-      return data.images.map((i: string) => `${USER_CONTENTS_HOST}/public/images/${chapter.fid}/${i}`)
+      return data.images.map((i: string) => `${USER_CONTENTS_HOST}/images/${chapter.fid}/${i}`)
     } else {
       throw new Error()
     }
@@ -45,17 +45,6 @@ export default async function ChapterPage({ params }: Props) {
   const chapterId = getSlugId(params.slug)
   const chapter = await getChapter(chapterId)
   if (!chapter) return notFound()
-
-  // if (chapter.mobileOnly && chapter.type === "comic")
-  //   return (
-  //     <div className="container px-4 xl:max-w-6xl">
-  //       <div className="border border-dashed px-4 py-8 flex flex-col gap-4 items-center justify-center rounded-xl">
-  //         <p className="text-lg font-semibold text-center">
-  //           Nội dung không khả dụng ngay bây giờ, vui lòng quay lại sau. Xin cám ơn!
-  //         </p>
-  //       </div>
-  //     </div>
-  //   )
 
   const images: string[] = await getImages(chapter)
 
@@ -82,8 +71,7 @@ export default async function ChapterPage({ params }: Props) {
         ) : chapter.type === "comic" ? (
           <div className="-mx-4 sm:mx-auto max-w-3xl border rounded-xl overflow-hidden">
             {images.map((img, index) => {
-              // const { pathname, search } = new URL(img)
-              return <Image alt="" src={img} effect="blur" tmpRatio="1/1" threshold={1200} key={chapter.id + index} />
+              return <Image alt="" src={img} effect="blur" tmpRatio="1/1" threshold={2400} key={chapter.id + index} />
             })}
           </div>
         ) : (
