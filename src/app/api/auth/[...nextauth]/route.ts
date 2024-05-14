699a5a7006aa25bef58ee63d8f8394c39c1458f9
pencilbsp <pencil.bsp@gmail.com>
2024-05-14 14:00:06 +0700

    fix auth in next 14

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,48 +1,7 @@
-import NextAuth, { AuthOptions } from "next-auth"
-import GoogleProvider from "next-auth/providers/google"
-import FacebookProvider from "next-auth/providers/facebook"
+import NextAuth from "next-auth";
 
-import { PrismaAdapter } from "@next-auth/prisma-adapter"
+import authOptions from "@/lib/auth";
 
-import prisma from "@/lib/prisma"
-import {
-  GOOGLE_CLIENT_ID,
-  NEXT_AUTH_SECRET,
-  FACEBOOK_CLIENT_ID,
-  GOOGLE_CLIENT_SECRET,
-  FACEBOOK_CLIENT_SECRET,
-} from "@/config"
+const handler = NextAuth(authOptions);
 
-export const authOptions: AuthOptions = {
-  secret: NEXT_AUTH_SECRET,
-  providers: [
-    GoogleProvider({
-      clientId: GOOGLE_CLIENT_ID,
-      clientSecret: GOOGLE_CLIENT_SECRET,
-    }),
-    FacebookProvider({
-      clientId: FACEBOOK_CLIENT_ID,
-      clientSecret: FACEBOOK_CLIENT_SECRET,
-    }),
-  ],
-  adapter: PrismaAdapter(prisma as any),
-  callbacks: {
-    async session({ session, user }) {
-      return Promise.resolve({
-        ...session,
-        user: {
-          ...session.user,
-          id: user.id,
-        },
-      })
-    },
-  },
-  pages: {
-    signIn: "/login",
-  },
-}
-
-const handler = NextAuth(authOptions)
-
-// @ts-ignore
-export { handler as GET, handler as POST }
+export { handler as GET, handler as POST };
---
+++ src/lib/auth.ts
@@ -0,0 +1,45 @@
+import NextAuth, { AuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import FacebookProvider from "next-auth/providers/facebook";
+
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+
+import prisma from "@/lib/prisma";
+import {
+  GOOGLE_CLIENT_ID,
+  NEXT_AUTH_SECRET,
+  FACEBOOK_CLIENT_ID,
+  GOOGLE_CLIENT_SECRET,
+  FACEBOOK_CLIENT_SECRET,
+} from "@/config";
+
+const authOptions: AuthOptions = {
+  secret: NEXT_AUTH_SECRET,
+  providers: [
+    GoogleProvider({
+      clientId: GOOGLE_CLIENT_ID,
+      clientSecret: GOOGLE_CLIENT_SECRET,
+    }),
+    FacebookProvider({
+      clientId: FACEBOOK_CLIENT_ID,
+      clientSecret: FACEBOOK_CLIENT_SECRET,
+    }),
+  ],
+  adapter: PrismaAdapter(prisma as any),
+  callbacks: {
+    async session({ session, user }) {
+      return Promise.resolve({
+        ...session,
+        user: {
+          ...session.user,
+          id: user.id,
+        },
+      });
+    },
+  },
+  pages: {
+    signIn: "/login",
+  },
+};
+
+export default authOptions;
