0cd7c3cbc794a8c2c03e30eb12abeea5aa5670f4
pencilbsp <pencil.bsp@gmail.com>
2024-05-26 17:19:44 +0700

    fix

--- src/lib/format-number.ts
+++ src/lib/format-number.ts
@@ -19,6 +19,41 @@ numeral.register("locale", "vi-custom", {
   },
 });
 
+numeral.register("format", "customFormat", {
+  regexps: {
+    format: /(\d+)(\.\d{1,2})?[a-z]?/,
+    unformat: /(\d+)(\.\d{1,2})?[a-z]?/,
+  },
+  format: function (value, format, roundingFunction) {
+    let formattedValue;
+    if (value < 1000) {
+      formattedValue = numeral._.numberToFormat(value, "0", roundingFunction); // Không có chữ số thập phân
+    } else {
+      formattedValue = numeral._.numberToFormat(
+        value,
+        "0.0a",
+        roundingFunction
+      );
+    }
+    if (formattedValue.includes("K")) {
+      return formattedValue.replace("K", " N");
+    } else if (formattedValue.includes("M")) {
+      return formattedValue.replace("M", " Tr");
+    } else {
+      return formattedValue;
+    }
+  },
+  unformat: function (string) {
+    if (string.includes(" N")) {
+      return numeral._.stringToNumber(string.replace(" N", "K"));
+    } else if (string.includes(" Tr")) {
+      return numeral._.stringToNumber(string.replace(" Tr", "M"));
+    } else {
+      return numeral._.stringToNumber(string);
+    }
+  },
+});
+
 numeral.locale("vi-custom");
 
 export default numeral;
