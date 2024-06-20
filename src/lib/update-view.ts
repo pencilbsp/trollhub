5a0536bc002d2dcc07ea98fb3289e81729737440
pencilbsp <pencil.bsp@gmail.com>
2024-06-21 00:34:16 +0700

    add update view tool

--- src/lib/update-view.ts
+++ src/lib/update-view.ts
@@ -6,68 +6,107 @@ import getRedisClient, { getKeyWithNamespace, type RedisClient } from "./redis";
 type ViewType = "content" | "chapter";
 // type View = { type: ViewType; view: number };
 
-async function getViewKeys(redisClient: RedisClient, match = "view_*") {
-  let cursor = 0;
-  const keys = [];
+export async function getViewKeys(redisClient: RedisClient, match = "view_*") {
+    let cursor = 0;
+    const keys = [];
 
-  do {
-    const data = await redisClient.scan(cursor, {
-      COUNT: 1000,
-      MATCH: match,
-    });
+    do {
+        const data = await redisClient.scan(cursor, {
+            COUNT: 1000,
+            MATCH: match,
+        });
 
-    cursor = data.cursor;
-    keys.push(...data.keys);
-  } while (cursor !== 0);
+        cursor = data.cursor;
+        keys.push(...data.keys);
+    } while (cursor !== 0);
 
-  return keys;
+    return keys;
 }
-
 
 export default async function updateView(contentId: string, type: ViewType) {
-  try {
-    const redisClient = await getRedisClient();
+    try {
+        const redisClient = await getRedisClient();
 
-    const redisKey = getKeyWithNamespace("view", type, contentId);
-    let view: string | number | null = await redisClient.get(redisKey);
+        const redisKey = getKeyWithNamespace("view", type, contentId);
+        let view: string | number | null = await redisClient.get(redisKey);
 
-    if (!view) {
-      //  @ts-ignore
-      // const content = await prisma[type].findUnique({
-      //   where: { id: contentId },
-      //   select: { view: true },
-      // });
+        if (!view) {
+            //  @ts-ignore
+            // const content = await prisma[type].findUnique({
+            //   where: { id: contentId },
+            //   select: { view: true },
+            // });
 
-      // view = { type, view: content.view };
-      view = 0;
-    } else {
-      view = Number(view);
+            // view = { type, view: content.view };
+            view = 0;
+        } else {
+            view = Number(view);
+        }
+
+        view++;
+        // console.log(view);
+
+        await redisClient.set(redisKey, view.toString());
+    } catch (error) {
+        console.log("Update View Error:", contentId, error);
     }
-
-    view++;
-    // console.log(view);
-
-    await redisClient.set(redisKey, view.toString());
-  } catch (error) {
-    console.log("Update View Error:", contentId, error);
-  }
 }
 
-
 export async function getContentMostViews(type: "content" | "chapter") {
-  const redisClient = await getRedisClient();
-  const keys = await getViewKeys(redisClient, `view_${type}_*`);
+    const redisClient = await getRedisClient();
+    const keys = await getViewKeys(redisClient, `view_${type}_*`);
 
-  const views: { id: string; view: number }[] = [];
+    const views: { id: string; view: number }[] = [];
 
-  do {
-    try {
-      const view = await redisClient.get(keys[0]);
-      views.push({ id: keys[0].split("_")[2], view: Number(view) });
-    } catch (error) { }
+    do {
+        try {
+            const view = await redisClient.get(keys[0]);
+            views.push({ id: keys[0].split("_")[2], view: Number(view) });
+        } catch (error) {}
 
-    keys.shift();
-  } while (keys.length !== 0);
+        keys.shift();
+    } while (keys.length !== 0);
 
-  return views.sort((a, b) => b.view - a.view);
+    return views.sort((a, b) => b.view - a.view);
+}
+
+export async function updateContentView(redisClient: RedisClient, key: string) {
+    const id = key.split("_")[2];
+    const type = key.split("_")[1];
+
+    const count = await redisClient.get(key);
+
+    if (count && id) {
+        // @ts-ignore
+        let content = await prisma[type].findUnique({
+            where: {
+                id,
+            },
+            select: {
+                view: true,
+                updatedAt: true,
+            },
+        });
+
+        if (content) {
+            // @ts-ignore
+            content = await prisma[type].update({
+                where: {
+                    id,
+                },
+                data: {
+                    view: {
+                        increment: Number(count),
+                    },
+                    updatedAt: content.updatedAt,
+                },
+                select: {
+                    fid: true,
+                    view: true,
+                    title: true,
+                    updatedAt: true,
+                },
+            });
+        }
+    }
 }
---
+++ src/update-view.ts
@@ -0,0 +1,45 @@
+import Queue from "bull";
+import getRedisClient from "./lib/redis";
+import { getViewKeys, updateContentView } from "./lib/update-view";
+
+declare global {
+    var updateViewCron: Queue.Queue<any>;
+}
+
+export default function updateViewCron(cron?: string) {
+    if (!cron) return;
+    if (globalThis.updateViewCron) return;
+
+    // Update view vào 00:00 hàng ngày
+    const updateViewQueue = new Queue("updateViewQueue");
+    globalThis.updateViewCron = updateViewQueue;
+
+    updateViewQueue.process(async (job) => {
+        const redisClient = await getRedisClient();
+        const keys = await getViewKeys(redisClient);
+
+        do {
+            try {
+                await updateContentView(redisClient, keys[0]);
+                await redisClient.del(keys[0]);
+            } catch (error) {
+                console.warn(keys[0], error);
+            }
+
+            keys.shift();
+        } while (keys.length !== 0);
+    });
+
+    updateViewQueue.add(
+        {},
+        {
+            repeat: { cron },
+        }
+    );
+
+    updateViewQueue.on("completed", (job, result) => {});
+
+    updateViewQueue.on("failed", (job, err) => {
+        console.log(`Cập nhật lượt xem ${job.id} đã thất bại! Lỗi: ${err}`);
+    });
+}
