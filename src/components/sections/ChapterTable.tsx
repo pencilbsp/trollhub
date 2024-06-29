b9718ac550264aeaf1e8806e4606e2180fd505df
pencilbs <pencil.bsp@gmail.com>
2024-06-29 22:11:17 +0700

    fix flex

--- src/components/sections/ChapterTable.tsx
+++ src/components/sections/ChapterTable.tsx
@@ -15,187 +15,235 @@ import useChapters from "@/hooks/useChapters";
 
 import { Card } from "@/components/ui/Card";
 import { Badge } from "@/components/ui/Badge";
-import { Table, TableRow, TableBody, TableCell, TableHead, TableHeader } from "@/components/ui/Table";
-import { Select, SelectItem, SelectValue, SelectContent, SelectTrigger } from "@/components/ui/Select";
+import {
+  Table,
+  TableRow,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+} from "@/components/ui/Table";
+import {
+  Select,
+  SelectItem,
+  SelectValue,
+  SelectContent,
+  SelectTrigger,
+} from "@/components/ui/Select";
 
 interface Props {
-    contentId: string;
-    data?: ChapterList;
-    currentId?: string;
-    contentTitle: string;
-    contentType: ContentType;
-    createdAt?: Prisma.SortOrder;
-    hiddenColumns?: ("view" | "update")[];
+  contentId: string;
+  data?: ChapterList;
+  currentId?: string;
+  contentTitle: string;
+  contentType: ContentType;
+  createdAt?: Prisma.SortOrder;
+  hiddenColumns?: ("view" | "update")[];
 }
 
 export default function ChapterTable({
-    data,
-    contentId,
-    createdAt,
-    currentId,
-    contentType,
-    contentTitle,
-    hiddenColumns,
+  data,
+  contentId,
+  createdAt,
+  currentId,
+  contentType,
+  contentTitle,
+  hiddenColumns,
 }: Props) {
-    if (!createdAt) createdAt = "desc";
-    if (!hiddenColumns) hiddenColumns = [];
+  if (!createdAt) createdAt = "desc";
+  if (!hiddenColumns) hiddenColumns = [];
 
+  const { chapters, mutate } = useChapters(
+    contentId,
     // @ts-ignore
-    const { chapters, mutate } = useChapters(contentId, data ? { data: data, total: data.length } : undefined);
+    data ? { data: data, total: data.length } : undefined
+  );
 
-    const handleSort = (value: Prisma.SortOrder) => {
-        mutate(
-            // @ts-ignore
-            [...chapters.sort((a, b) => {
-                if (value === "desc") [a, b] = [b, a];
-                return a.createdAt.getTime() - b.createdAt.getTime();
-            })],
-            { revalidate: false }
-        );
-    };
-
-    return (
-        <div>
-            <div className="flex items-center justify-between">
-                <h3 className="font-bold text-xl uppercase">
-                    {contentType === "movie" ? "Danh sách tập" : "Danh sách chương"}
-                </h3>
-                <Select defaultValue={createdAt} onValueChange={handleSort}>
-                    <SelectTrigger className="w-28 h-8">
-                        <SelectValue placeholder="Sắp xếp" />
-                    </SelectTrigger>
-                    <SelectContent align="end">
-                        <SelectItem value="desc">Mới nhất</SelectItem>
-                        <SelectItem value="asc">Cũ nhất</SelectItem>
-                    </SelectContent>
-                </Select>
-            </div>
-            <Card className="mt-4">
-                <div className="max-h-80 overflow-y-auto">
-                    {/* @ts-ignore */}
-                    {chapters.length > 0 && (
-                        <Table>
-                            <TableHeader className="sticky top-0 dark:bg-gray-950 bg-gray-50 mx-4">
-                                <TableRow>
-                                    <TableHead>Tên</TableHead>
-                                    {!hiddenColumns.includes("update") && (
-                                        <TableHead className="text-right truncate">Cập nhật</TableHead>
-                                    )}
-                                    {!hiddenColumns.includes("view") && (
-                                        <TableHead className="text-right truncate">Lượt xem</TableHead>
-                                    )}
-                                </TableRow>
-                            </TableHeader>
-                            <TableBody>
-                                {/* @ts-ignore */}
-                                {chapters.map(({ id, title, createdAt, mobileOnly, type, view }) => {
-                                    const isActive = id === currentId;
-                                    const slugTitle = `${contentTitle}-${title}`;
-                                    const href = `/${type === ContentType.movie ? "episode" : "chapter"}/${slug(
-                                        slugTitle
-                                    )}-${id}`;
-
-                                    return (
-                                        <ChapterRow
-                                            key={id}
-                                            view={view}
-                                            href={href}
-                                            title={title!}
-                                            isActive={isActive}
-                                            createdAt={createdAt}
-                                            mobileOnly={mobileOnly}
-                                            hiddenColumns={hiddenColumns}
-                                        />
-                                    );
-                                })}
-                            </TableBody>
-                        </Table>
-                    )}
-                </div>
-            </Card>
-        </div>
+  const handleSort = (value: Prisma.SortOrder) => {
+    mutate(
+      // @ts-ignore
+      [
+        ...chapters.sort((a, b) => {
+          if (value === "desc") [a, b] = [b, a];
+          return a.createdAt.getTime() - b.createdAt.getTime();
+        }),
+      ],
+      { revalidate: false }
     );
+  };
+
+  return (
+    <div>
+      <div className="flex items-center justify-between">
+        <h3 className="font-bold text-xl uppercase">
+          {contentType === "movie" ? "Danh sách tập" : "Danh sách chương"}
+        </h3>
+        <Select defaultValue={createdAt} onValueChange={handleSort}>
+          <SelectTrigger className="w-28 h-8">
+            <SelectValue placeholder="Sắp xếp" />
+          </SelectTrigger>
+          <SelectContent align="end">
+            <SelectItem value="desc">Mới nhất</SelectItem>
+            <SelectItem value="asc">Cũ nhất</SelectItem>
+          </SelectContent>
+        </Select>
+      </div>
+      <Card className="mt-4">
+        <div className="max-h-80 overflow-y-auto">
+          {/* @ts-ignore */}
+          {chapters.length > 0 && (
+            <Table>
+              <TableHeader className="sticky top-0 dark:bg-gray-950 bg-gray-50 mx-4">
+                <TableRow>
+                  <TableHead>Tên</TableHead>
+                  {!hiddenColumns.includes("update") && (
+                    <TableHead className="text-right truncate">
+                      Cập nhật
+                    </TableHead>
+                  )}
+                  {!hiddenColumns.includes("view") && (
+                    <TableHead className="text-right truncate">
+                      Lượt xem
+                    </TableHead>
+                  )}
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                {/* @ts-ignore */}
+                {chapters.map(
+                  ({ id, title, createdAt, mobileOnly, type, view }) => {
+                    const isActive = id === currentId;
+                    const slugTitle = `${contentTitle}-${title}`;
+                    const href = `/${
+                      type === ContentType.movie ? "episode" : "chapter"
+                    }/${slug(slugTitle)}-${id}`;
+
+                    return (
+                      <ChapterRow
+                        key={id}
+                        view={view}
+                        href={href}
+                        title={title!}
+                        isActive={isActive}
+                        createdAt={createdAt}
+                        mobileOnly={mobileOnly}
+                        hiddenColumns={hiddenColumns}
+                      />
+                    );
+                  }
+                )}
+              </TableBody>
+            </Table>
+          )}
+        </div>
+      </Card>
+    </div>
+  );
 }
 
 function scrollParentToChild(parent: HTMLElement, child: HTMLElement) {
-    // Where is the parent on page
-    const parentRect = parent.getBoundingClientRect();
-    // What can you see?
-    const parentViewableArea = {
-        height: parent.clientHeight,
-        width: parent.clientWidth,
-    };
+  // Where is the parent on page
+  const parentRect = parent.getBoundingClientRect();
+  // What can you see?
+  const parentViewableArea = {
+    height: parent.clientHeight,
+    width: parent.clientWidth,
+  };
 
-    // Where is the child
-    const childRect = child.getBoundingClientRect();
-    // Is the child viewable?
-    const isViewable =
-        childRect.top >= parentRect.top && childRect.bottom <= parentRect.top + parentViewableArea.height;
+  // Where is the child
+  const childRect = child.getBoundingClientRect();
+  // Is the child viewable?
+  const isViewable =
+    childRect.top >= parentRect.top &&
+    childRect.bottom <= parentRect.top + parentViewableArea.height;
 
-    // if you can't see the child try to scroll parent
-    if (!isViewable) {
-        // Should we scroll using top or bottom? Find the smaller ABS adjustment
-        const scrollTop = childRect.top - parentRect.top;
-        const scrollBot = childRect.bottom - parentRect.bottom;
-        if (Math.abs(scrollTop) < Math.abs(scrollBot)) {
-            // we're near the top of the list
-            parent.scrollTop += scrollTop;
-        } else {
-            // we're near the bottom of the list
-            parent.scrollTop += scrollBot;
-        }
+  // if you can't see the child try to scroll parent
+  if (!isViewable) {
+    // Should we scroll using top or bottom? Find the smaller ABS adjustment
+    const scrollTop = childRect.top - parentRect.top;
+    const scrollBot = childRect.bottom - parentRect.bottom;
+    if (Math.abs(scrollTop) < Math.abs(scrollBot)) {
+      // we're near the top of the list
+      parent.scrollTop += scrollTop;
+    } else {
+      // we're near the bottom of the list
+      parent.scrollTop += scrollBot;
     }
+  }
 }
 
 type ChapterRowProps = {
-    href: string;
-    view: number | null;
-    title: string;
-    isActive: boolean;
-    mobileOnly: boolean;
-    createdAt: Date;
-    hiddenColumns?: ("view" | "update")[];
+  href: string;
+  view: number | null;
+  title: string;
+  isActive: boolean;
+  mobileOnly: boolean;
+  createdAt: Date;
+  hiddenColumns?: ("view" | "update")[];
 };
 
-function ChapterRow({ href, isActive, mobileOnly, title, view, hiddenColumns, createdAt }: ChapterRowProps) {
-    const rowRef = useRef<HTMLTableRowElement>(null);
+function ChapterRow({
+  href,
+  isActive,
+  mobileOnly,
+  title,
+  view,
+  hiddenColumns,
+  createdAt,
+}: ChapterRowProps) {
+  const rowRef = useRef<HTMLTableRowElement>(null);
 
-    useEffect(() => {
-        if (rowRef.current && isActive) {
-            rowRef.current.scrollIntoView({
-                behavior: "smooth",
-                block: "nearest",
-                inline: "nearest",
-            });
-        }
-    }, [isActive]);
+  useEffect(() => {
+    if (rowRef.current && isActive) {
+      rowRef.current.scrollIntoView({
+        behavior: "smooth",
+        block: "nearest",
+        inline: "nearest",
+      });
+    }
+  }, [isActive]);
 
-    return (
-        <TableRow ref={rowRef}>
-            <TableCell className="font-medium font-mono max-w-md">
-                <Link href={href} className={cn("flow-root max-w-full", isActive && "text-blue-500")}>
-                    <div className="flex overflow-hidden">
-                        <span className="truncate">{title.trim()}</span>
-                        {mobileOnly && <TabletSmartphoneIcon size={16} className="ml-2 text-red-400 inline-block flex-shrink-0" />}
-                    </div>
-                    {isActive && (
-                        <Badge
-                            variant="outline"
-                            className="ml-2 px-1.5 font-sans border-blue-500/70 bg-blue-500/90 text-background"
-                        >
-                            Đang xem
-                        </Badge>
-                    )}
-                </Link>
-            </TableCell>
-            {!hiddenColumns!.includes("update") && (
-                <TableCell className="text-right">
-                    <time className="font-mono font-light text-sm truncate">{formatDate(createdAt)}</time>
-                </TableCell>
+  return (
+    <TableRow ref={rowRef}>
+      <TableCell className="font-medium font-mono max-w-md">
+        <Link
+          href={href}
+          className={cn(
+            "flex items-center max-w-full",
+            isActive && "text-blue-500"
+          )}
+        >
+          <div className="flex overflow-hidden">
+            <span className="truncate">{title.trim()}</span>
+            {mobileOnly && (
+              <TabletSmartphoneIcon
+                size={16}
+                className="ml-2 text-red-400 inline-block flex-shrink-0"
+              />
             )}
-            {!hiddenColumns!.includes("update") && (
-                <TableCell className="font-mono text-right">{numeral(view || 0).format("0.0a")}</TableCell>
-            )}
-        </TableRow>
-    );
+          </div>
+          {isActive && (
+            <Badge
+              variant="outline"
+              className="ml-2 px-1.5 font-sans border-blue-500/70 bg-blue-500/90 text-background"
+            >
+              Đang xem
+            </Badge>
+          )}
+        </Link>
+      </TableCell>
+      {!hiddenColumns!.includes("update") && (
+        <TableCell className="text-right">
+          <time className="font-mono font-light text-sm truncate">
+            {formatDate(createdAt)}
+          </time>
+        </TableCell>
+      )}
+      {!hiddenColumns!.includes("update") && (
+        <TableCell className="font-mono text-right">
+          {numeral(view || 0).format("0.0a")}
+        </TableCell>
+      )}
+    </TableRow>
+  );
 }
