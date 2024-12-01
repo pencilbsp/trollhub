09d2e8535f5af85d53880d8b32b89d0d5ca27922
pencilbsp <pencil.bsp@gmail.com>
2024-12-01 23:23:53 +0700

    update

--- src/components/sections/LoadMoreContent.tsx
+++ src/components/sections/LoadMoreContent.tsx
@@ -16,7 +16,7 @@ export default function LoadMoreContent({ creatorId, skip, take }: Props) {
     return (
         <div className="flex flex-col items-center mt-4 w-full gap-y-6">
             {contents.length > 0 && (
-                <div className="grid grid-cols-2 grid-rows-4 sm:grid-cols-3 sm:grid-rows-3 lg:grid-cols-4 lg:grid-rows-2 gap-4 md:gap-2 xl:gap-4 w-full">
+                <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4 md:gap-2 xl:gap-4 w-full">
                     {contents.map((content: any) => (
                         <ContentVertical key={content.id} data={content} />
                     ))}
