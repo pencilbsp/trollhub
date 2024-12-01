bb98ff2cd9bb9517a15ffb1a81dfe2c3787a7971
pencilbsp <pencil.bsp@gmail.com>
2024-12-01 21:14:54 +0700

    fix

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -15,7 +15,7 @@ export default async function Home() {
     let homeData = await redisClient.json<HomeData>(redisKey);
 
     if (!homeData) {
-        const response = await fetch(USER_CONTENTS_HOST + '/api/home-data');
+        const response = await fetch(USER_CONTENTS_HOST + '/api/home-data', { cache: 'no-store' });
         homeData = (await response.json()) as HomeData;
         await redisClient.set(redisKey, JSON.stringify(homeData), { EX: HOME_EX_TIME });
     }
